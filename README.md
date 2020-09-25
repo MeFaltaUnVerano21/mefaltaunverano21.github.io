8185b93612b2314ae7b5c4effd6c22f1bbf36d75
MeFaltaUnVerano <trolleros200@gmail.com>
2020-09-25 22:47:05 +0200

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# mefaltaunverano-web
-This is my first project with, html, css and react. I know that react could be a bit overpower for this website but is only for testing and education propuse.
---
+++ precache-manifest.953f97ab854a106735c83b7cd41a5555.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bce56497465f669103d3e3b6d61043a3",
+    "url": "/mefaltaunverano-web/index.html"
+  },
+  {
+    "revision": "f5d4955b8b15184265e9",
+    "url": "/mefaltaunverano-web/static/css/main.e6ef48d6.chunk.css"
+  },
+  {
+    "revision": "f45b3dbedc30fef67421",
+    "url": "/mefaltaunverano-web/static/js/2.52edec0d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mefaltaunverano-web/static/js/2.52edec0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5d4955b8b15184265e9",
+    "url": "/mefaltaunverano-web/static/js/main.a831f4e0.chunk.js"
+  },
+  {
+    "revision": "a6d248b7fa4a20dfa60e",
+    "url": "/mefaltaunverano-web/static/js/runtime-main.d6435de9.js"
+  },
+  {
+    "revision": "7454bcaa29f735c9e1fce8cac53cbbf7",
+    "url": "/mefaltaunverano-web/static/media/logo.7454bcaa.png"
+  }
+]);
